f8ddf34d78c4e597cf01c37fc1c589b7028fcd40
Serhii-Buha <serhii.buga@gmail.com>
2023-05-13 12:48:09 +0300

    add prop-types and styles

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+`;
+
+// export const Container = styled.div`
+//   max-width: 1168px;
+//   margin: 0 auto;
+//   padding: 16px 16px;
+// `;
